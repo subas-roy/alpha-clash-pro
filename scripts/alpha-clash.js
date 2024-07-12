f879eedf3e9753cfe9566530b671d24ff5912368
subas-roy <subasroy46@gmail.com>
2024-07-13 05:18:42 +0600

    Update Score and Life based on right or wrong key press

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -21,14 +21,31 @@ function handleKeyboardKeyUpEvent(event){
         console.log('you get a  point');
         // update score
         // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
         // 2. increase the score by 1
+        const newScore = currentScore + 1;
+
         // 3. show the updated score
+        currentScoreElement.innerText = newScore;
 
         // start a new round
         removeBackgroundColorById(expectedAlphabet)
         continueGame();
     }else{
         console.log('you missed!, you lost a life')
+        // get the current life numner
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // decrease the life
+        const newLife = currentLife - 1;
+
+        // display the life count
+        currentLifeElement.innerText = newLife;
     }
 }
 document.addEventListener('keyup', handleKeyboardKeyUpEvent);
